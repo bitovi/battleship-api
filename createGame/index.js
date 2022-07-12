47939ddd143da523f5b0157d99b5e474f99b16fc
Austin Kurpuis <austin@bitovi.com>
2022-07-12 12:19:56 -0700

    Setup dynamo test code

--- createGame/index.js
+++ createGame/index.js
@@ -1,14 +1,27 @@
 "use strict";
 
+const { getDefaultRoleAssumerWithWebIdentity } = require("@aws-sdk/client-sts");
+const { defaultProvider } = require("@aws-sdk/credential-provider-node");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const client = new DynamoDB({ region: "us-east-1" });
+
+const credentialProvider = defaultProvider({
+  roleAssumerWithWebIdentity: getDefaultRoleAssumerWithWebIdentity,
+});
+
+const client = new DynamoDB({
+  region: "us-east-1",
+  credentialDefaultProvider: credentialProvider
+});
 
 module.exports.handler = async (event) => {
   const results = await client.listTables({});
   return {
     statusCode: 200,
     body: JSON.stringify(
-      results,
+      {
+        results,
+        GAMES_TABLE_NAME: process.env.GAMES_TABLE_NAME
+      },
       null,
       2
     ),
