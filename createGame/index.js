3af678d9dcc0a15af920fd252e944ced4914fec1
Austin Kurpuis <austin@bitovi.com>
2022-07-12 12:39:16 -0700

    Test record creation

--- createGame/index.js
+++ createGame/index.js
@@ -3,25 +3,31 @@
 const { getDefaultRoleAssumerWithWebIdentity } = require("@aws-sdk/client-sts");
 const { defaultProvider } = require("@aws-sdk/credential-provider-node");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const { v4: createUuid } = require("uuid");
+const { AWS_REGION, GAMES_TABLE_NAME } = process.env;
 
 const credentialProvider = defaultProvider({
   roleAssumerWithWebIdentity: getDefaultRoleAssumerWithWebIdentity,
 });
 
 const client = new DynamoDB({
-  region: "us-east-1",
+  region: AWS_REGION,
   credentialDefaultProvider: credentialProvider
 });
 
 module.exports.handler = async (event) => {
-  const results = await client.listTables({});
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        results,
-        GAMES_TABLE_NAME: process.env.GAMES_TABLE_NAME
+  const result = await client.putItem({
+    TableName: GAMES_TABLE_NAME,
+      Item: {
+        id: createUuid(),
+        message: "Hello World!"
       },
+  });
+
+  return {
+    statusCode: $metadata.httpStatusCode,
+    body: JSON.stringify(
+      result,
       null,
       2
     ),
