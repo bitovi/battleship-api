9ec61ca2e6e64b996dcd9e19bd3c274b126ecdbc
Nauany Cardoso <nauanycc@outlook.com.br>
2022-07-13 13:49:32 -0300

    style: run prettier

--- createGame/index.js
+++ createGame/index.js
@@ -1,21 +1,23 @@
-"use strict";
+'use strict';
 
-const { getDefaultRoleAssumerWithWebIdentity } = require("@aws-sdk/client-sts");
-const { defaultProvider } = require("@aws-sdk/credential-provider-node");
-const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const { v4: createUuid } = require("uuid");
+const { getDefaultRoleAssumerWithWebIdentity } = require('@aws-sdk/client-sts');
+const { defaultProvider } = require('@aws-sdk/credential-provider-node');
+const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { v4: createUuid } = require('uuid');
 const { AWS_REGION, GAMES_TABLE_NAME } = process.env;
-const createError = require("http-errors");
+const createError = require('http-errors');
 
 const credentialProvider = defaultProvider({
   roleAssumerWithWebIdentity: getDefaultRoleAssumerWithWebIdentity,
 });
 
-const dynamoClient = DynamoDBDocument.from(new DynamoDB({
-  region: AWS_REGION,
-  credentialDefaultProvider: credentialProvider
-}));
+const dynamoClient = DynamoDBDocument.from(
+  new DynamoDB({
+    region: AWS_REGION,
+    credentialDefaultProvider: credentialProvider,
+  })
+);
 
 module.exports.handler = async (event) => {
   const id = createUuid();
@@ -23,16 +25,16 @@ module.exports.handler = async (event) => {
     TableName: GAMES_TABLE_NAME,
     Item: {
       id,
-      message: "Hello World!"
-    }
+      message: 'Hello World!',
+    },
   });
 
   const documentGetResult = await dynamoClient.get({
     TableName: GAMES_TABLE_NAME,
     Key: {
-      id
-    }
-  })
+      id,
+    },
+  });
 
   if (!documentGetResult.Item) {
     throw createError(500);
@@ -40,10 +42,6 @@ module.exports.handler = async (event) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      documentGetResult.Item,
-      null,
-      2
-    )
+    body: JSON.stringify(documentGetResult.Item, null, 2),
   };
 };
