e08ad75c0dca743a659c9cee38a35f39f27400a6
Mark Repka <mrepka@bitovi.com>
2022-07-13 16:48:03 -0400

    Fix linting

--- placeShip/index.js
+++ placeShip/index.js
@@ -1,30 +1,6 @@
 "use strict";
 
-const { v4: createUuid } = require("uuid");
-const { GAMES_TABLE_NAME } = process.env;
-const createError = require("http-errors");
-const dynamoClient = require("../common.js").dynamo;
-
-module.exports.handler = async (event) => {
-  // await dynamoClient.put({
-  //   TableName: GAMES_TABLE_NAME,
-  //   Item: {
-  //     id,
-  //     message: "Hello Worlds!"
-  //   }
-  // });
-
-  // const documentGetResult = await dynamoClient.get({
-  //   TableName: GAMES_TABLE_NAME,
-  //   Key: {
-  //     id
-  //   }
-  // })
-
-  // if (!documentGetResult.Item) {
-  //   throw createError(500);
-  // }
-
+module.exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ result: "Hello World!" })
