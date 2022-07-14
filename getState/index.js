30299bdd89b003f8782c6d80c8df649b3852fcd2
BS <bens640@gmail.com>
2022-07-14 15:24:53 -0400

    a

--- getState/index.js
+++ getState/index.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const { v4: createUuid } = require("uuid");
 const { GAMES_TABLE_NAME } = process.env;
 const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
@@ -8,7 +7,6 @@ const dynamoClient = require("../common.js").dynamo;
 const privateKey = require("../common.js").privateKey;
 
 module.exports.handler = async (event) => {
-    const id = createUuid();
     let body;
     try {
         body = JSON.parse(event.body);
@@ -21,19 +19,19 @@ module.exports.handler = async (event) => {
         }
     }
 
-    const id = body.gameId
-
+    const id = body.id
+    console.log(body, '🩸')
     const documentGetResult = await dynamoClient.get({
         TableName: GAMES_TABLE_NAME,
         Key: {
             id
         }
     });
+    console.log(documentGetResult)
 
     if (!documentGetResult.Item) {
         throw createError(500);
     }
-
 
     return {
         statusCode: 200,
