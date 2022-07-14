8bf91182c1b8abe62b269d8f768cc24dcea3f3cb
BS <bens640@gmail.com>
2022-07-14 14:07:36 -0400

    added placeShip

--- placeShip/index.js
+++ placeShip/index.js
@@ -1,8 +1,97 @@
 "use strict";
 
-module.exports.handler = async () => {
+const { v4: createUuid } = require("uuid");
+const { GAMES_TABLE_NAME } = process.env;
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
+const dynamoClient = require("../common.js").dynamo;
+const privateKey = require("../common.js").privateKey;
+const {    isVerticalCheck,
+  getVaryingCord,
+  isOutOfBound,
+  isGreaterThanShipSize,} = require("../common.js")
+
+module.exports.handler = async (event) => {
+  let body;
+  let header = JSON.parse(event.header)
+  try {
+    body = JSON.parse(event.body);
+  } catch (err) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: "Bad Request"
+      })
+    }
+  }
+
+  const userId = jwt.verify(header.token, 'fish')
+
+  const gameId = body.gameId ?? '';  
+  //validate gameId
+  const documentGetResult = await dynamoClient.get({
+    TableName: GAMES_TABLE_NAME,
+    Key: {
+      id: gameId
+    }
+  })
+  if (!documentGetResult.Item) {
+    throw createError('Game ID Entered Does Not Exist');
+  }
+  const gridSize = documentGetResult.Item.gridSize
+  const shipSize = documentGetResult.Item.ships[0].shipSize  
+  if (isOutOfBound(body.coordinates,gridSize)) throw createError("Out of Bounds")
+  if (isGreaterThanShipSize(body.coordinates,shipSize)) throw createError("Ship is too big")
+  const isVertical = isVerticalCheck(body.coordinates)
+  const varyingCord = getVaryingCord(coordinates,isVertical)
+  const constCoord = isVertical ? body.coordinates[0].x : body.coordinates[0].y 
+  const gId = body.gameId;
+  const shipName = body.shipName;
+  let coordinates = []
+
+  const updatedPlayer = players.map(player => {
+    if(player.userId === userId) {
+        //update the 
+    }
+}) 
+
+
+  const userShip = {
+    shipName: shipName,
+    isVertical: isVertical,
+    constCoord: constCoord,
+    coordinates: coordinates,
+
+  }
+  const currentPlayer  =
+  await dynamoClient.put({
+    TableName: GAMES_TABLE_NAME,
+    Key: {
+      id: gameId
+    },
+    Item: {
+      ...documentGetResult.Item, 
+      players:[
+        ...documentGetResult.Item.players,
+        {
+          userId,
+          name: newUserName,
+          isAdmin: false,
+          coordinates: [],
+        }
+      ]
+    }
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ result: "Hello World!" })
+    body: JSON.stringify(
+      {
+        userId: userId,
+        gridSize: documentGetResult.Item.gridSize,
+      },
+      null,
+      2
+    )
   };
 };
